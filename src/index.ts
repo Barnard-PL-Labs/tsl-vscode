432f1ed474ec5951055adcedcd11909114f4657b
Raven Rothkopf <rgr2124@barnard.edu>
2022-10-03 13:04:50 -0400

    update warnings in assumption block

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { spawn } from 'child_process'
+import * as vscode from 'vscode'
 import { existsSync, mkdirSync, writeFileSync } from 'fs'
 import path from 'path'
-import { isContext } from 'vm'
 import { commands, window, workspace } from 'vscode'
+
+export const TEST_MENTION = 'update_mention';
+const open = '[';
+const close = ']';
+
 
 export enum CommandKeys {
   synth = 'extension.tsl-vscode.synth',
@@ -19,7 +24,7 @@ export const synthOptions = [
   'applicative',
 ]
 
-export function activate() {
+export function activate(context: vscode.ExtensionContext) {
   commands.registerCommand(CommandKeys.synth, () => {
     const editor = window.activeTextEditor
     if (!editor) {
@@ -53,7 +58,13 @@ export function activate() {
   })
 
   const vscode = require('vscode')
+  
+  //diagnostics
+  const testDiagnostics = vscode.languages.createDiagnosticCollection("test")
+  context.subscriptions.push(testDiagnostics)
+  subscribeToDocumentChanges(context, testDiagnostics);
 
+  //hover for operator info
   vscode.languages.registerHoverProvider('tsl', {
     provideHover(document, position, token) {
 
@@ -117,12 +128,82 @@ export function activate() {
         else if (word == "<-") {
           return new vscode.Hover({
             language: "tsl",
-            value: "formula: update \nusage: [fxn <- fxnTerm] || [outSignal <- inSignal]"
+            value: "formula: update \nusage: [outSignal <- fxnTerm] || [outSignal <- inSignal]"
         });
+        }
+        else if (word == 'assume'){
+          return new vscode.Hover({
+            language: "tsl",
+            value: "assume block\nassumptions about the environment"
+          })
+        }
+        else if (word == 'guarantee'){
+          return new vscode.Hover({
+            language: 'tsl',
+            value: "guarantee block\nspecifications of the system"
+          })
         }
     }
 });
+}
 
+//diagnostic functions
+export function subscribeToDocumentChanges(context: vscode.ExtensionContext, testDiagnostics: vscode.DiagnosticCollection): void {
+	if (vscode.window.activeTextEditor) {
+		refreshDiagnostics(vscode.window.activeTextEditor.document, testDiagnostics);
+	}
+	context.subscriptions.push(
+		vscode.window.onDidChangeActiveTextEditor(editor => {
+			if (editor) {
+				refreshDiagnostics(editor.document, testDiagnostics);
+			}
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, testDiagnostics))
+	);
+
+	context.subscriptions.push(
+		vscode.workspace.onDidCloseTextDocument(doc => testDiagnostics.delete(doc.uri))
+	);
+
+}
+
+export function refreshDiagnostics(doc: vscode.TextDocument, testDiagnostics: vscode.DiagnosticCollection): void {
+	const diagnostics: vscode.Diagnostic[] = [];
+
+	for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
+		const lineOfText = doc.lineAt(lineIndex);
+    if (lineOfText.text.includes('assume')){
+      for(let newLine = lineIndex; newLine < doc.lineCount; newLine++){
+        const newLineText = doc.lineAt(newLine);
+        if (newLineText.text.includes(open) && newLineText.text.includes(close)) {
+           diagnostics.push(createDiagnostic(doc, newLineText, newLine));
+        }
+        else if(newLineText.text.includes("guarantee")){
+          break
+        }
+      }
+    }
+    else{break}
+	}
+
+	testDiagnostics.set(doc.uri, diagnostics);
+}
+
+function createDiagnostic(doc: vscode.TextDocument, lineOfText: vscode.TextLine, lineIndex: number): vscode.Diagnostic {
+	// find where in the line of thet the 'emoji' is mentioned
+	const start = lineOfText.text.indexOf(open);
+  const end = lineOfText.text.indexOf(close);
+
+	// create range that represents, where in the document the word is
+	const range = new vscode.Range(lineIndex, start, lineIndex, end);
+
+	const diagnostic = new vscode.Diagnostic(range, "Updates generally don't go in the assumption block, and they never go on the right side of an implication!",
+		vscode.DiagnosticSeverity.Information);
+	diagnostic.code = TEST_MENTION;
+	return diagnostic;
 }
 
 export function deactivate() {
